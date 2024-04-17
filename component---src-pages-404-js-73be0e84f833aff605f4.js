34c3523effd1d6ebb3f5ee1944c72185952a73f5
Filip Mladenovic <fmladenovic@Filips-MacBook-Air.local>
2024-04-17 20:53:38 +0200

    site build

---
+++ component---src-pages-404-js-73be0e84f833aff605f4.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktvoj_majstor=self.webpackChunktvoj_majstor||[]).push([[125],{7412:function(e,t,n){n.r(t);var a=n(6540),l=n(5054);class s extends a.Component{render(){return a.createElement(l.A,null,a.createElement("h1",null,"Not Found"),a.createElement("p",null,"You just hit a page that doesn't exist..."))}}t.default=s}}]);
+//# sourceMappingURL=component---src-pages-404-js-73be0e84f833aff605f4.js.map
